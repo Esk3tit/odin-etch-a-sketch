42c5fd8930a4e81313742f9eb6d685660ecc3ee9
Khai Phan <khaiphn41@gmail.com>
2024-01-11 17:58:38 -0800

    Add paint on hover feature to div cells
    Used event delegation to only need one event handler on the container
    to color in cells, otherwise we would need an event handler/listener for
    every cell which will take up a lot of memory when they all do the same
    thing so we can just handle it in the parent with bubbling.

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ for (let i = 0; i < 16; i++) {
     }
     document.querySelector('.container').appendChild(row);
 }
+
+// Add hover effect so that when mouse passes over div, it changes color
+// Use event delegation to add event listener to container, then use if statement to check if target is a column
+// to prevent event listener from firing when mouse passes over row and not having event listeners for every column
+// which would be inefficient
+document.querySelector('.container').addEventListener('mouseover', function(e) {
+    if (e.target.classList.contains('column')) {
+        e.target.classList.add('hover');
+    }
+});

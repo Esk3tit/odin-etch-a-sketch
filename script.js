f0aff3ed1e8899103992f5cb49a8387b476cc816
Khai Phan <khaiphn41@gmail.com>
2024-01-11 19:50:45 -0800

    Add extra credit progressive darkening effect
    We want it to be completely black after 10 "iterations" so taking off
    10% of the color each time wouldn't work (ex. r * 0.9, g * 0.9, b * 0.9)
    since that just repeatedly take 10% off of the color so the rate at
    which it gets to black decreases as it gets darker since the rgb is
    closer to 0 and 10% of that small number is small. It doesn't actually
    guarantee that we always get to black within 10 interactions.
    
    What we actually need to do is subtract 10% off of white, the max
    rgb value each time, this is taking off a 10% step each interaction
    which means we will always get to black in 10 interactions
    regardless of color (ex. r - 25.5 [25.5 is 10% of 255 white]...)

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function generateGrid(n) {
         for (let j = 0; j < n; j++) {
             const column = document.createElement('div');
             column.classList.add('column');
+            column.style.backgroundColor = 'white';
             row.appendChild(column);
         }
         document.querySelector('.container').appendChild(row);
@@ -31,6 +32,25 @@ function fillRandomColor(e) {
     }
 }
 
+function darken(e) {
+    if (e.target.classList.contains('column')) {
+        const currentColor = e.target.style.backgroundColor;
+
+        if (currentColor === 'black' || currentColor === 'rgb(0, 0, 0)') {
+            return;
+        }
+
+        const rgbColor = currentColor === 'white' ? ['255', '255', '255'] : currentColor.match(/\d+/g);
+        const [r, g, b] = rgbColor.map(color => Number(color));
+
+        const newR = Math.max(0, Math.floor(r - 25.5));
+        const newG = Math.max(0, Math.floor(g - 25.5));
+        const newB = Math.max(0, Math.floor(b - 25.5));
+
+        e.target.style.backgroundColor = `rgb(${newR}, ${newG}, ${newB})` === `rgb(0, 0, 0)` ? 'black' : `rgb(${newR}, ${newG}, ${newB})`;
+    }
+}
+
 generateGrid(16);
 
 document.querySelector('.container').addEventListener('mouseover', fillBlack);
@@ -54,13 +74,22 @@ newGridBtn.addEventListener('click', function() {
 // if we select black then remove old event listener and add fillBlack back in
 const rainbowBtn = document.querySelector('#rainbow-btn');
 const blackBtn = document.querySelector('#black-btn');
+const darkenBtn = document.querySelector('#darken-btn');
 
 rainbowBtn.addEventListener('click', function() {
     document.querySelector('.container').removeEventListener('mouseover', fillBlack);
+    document.querySelector('.container').removeEventListener('mouseover', darken);
     document.querySelector('.container').addEventListener('mouseover', fillRandomColor);
 });
 
 blackBtn.addEventListener('click', function() {
     document.querySelector('.container').removeEventListener('mouseover', fillRandomColor);
+    document.querySelector('.container').removeEventListener('mouseover', darken);
     document.querySelector('.container').addEventListener('mouseover', fillBlack);
+});
+
+darkenBtn.addEventListener('click', function() {
+    document.querySelector('.container').removeEventListener('mouseover', fillRandomColor);
+    document.querySelector('.container').removeEventListener('mouseover', fillBlack);
+    document.querySelector('.container').addEventListener('mouseover', darken);
 });
